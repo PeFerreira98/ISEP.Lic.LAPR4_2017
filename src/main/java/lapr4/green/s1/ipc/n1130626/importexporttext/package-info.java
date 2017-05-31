cad061e57158738ee957b0bab9a30f5270c20d8e
Pedro Pereira <1130626@isep.ipp.pt>
2017-05-31 17:31:40 +0100

    #LAPR4E17DL-110 #comment Functional/Integration Tests Done

--- src/main/java/lapr4/green/s1/ipc/n1130626/importexporttext/package-info.java
+++ src/main/java/lapr4/green/s1/ipc/n1130626/importexporttext/package-info.java
@@ -33,10 +33,34 @@
  * use case tests (e.g., like in the TDD approach).<p>
  *
  * Import data from a text file Functional/Integration Test:<p>
- *      1. The user should start the CleanSheets Application<p>
+ * 1. The user should start the CleanSheets Application<p>
+ * 2. Selects the range of cells he wants to be used<p>
+ * 2. Selects the option Extensions<p>
+ * 3. Selects the option Import/Export Data<p>
+ * 4. Press the "Import from a text file" option to open the feature<p>
+ * 5. A window named "Import from a text file" should appear with data to be filled by the user<p>
+ * 6. Press the "Find" button to search a text file to import from<p>
+ * 7. On the label "Special character" the user writes in the textbox located at the right side of the label, the character which will be used to separate columns<p>
+ * 8. Check/Uncheck the option if the first line of the text should be treated as containing the header of the columns<p>
+ * 9. Press the "Import" button to import the data<p>
+ * 10. The cells selected by the user should have the data from the imported text file<p>
+ * 11. No error should occur<p>
+ * 
  * <p>
  *
  * Export data to a text file Functional/Integration Test:<p>
+ * 1. The user should start the CleanSheets Application<p>
+ * 2. Selects the range of cells he wants to be used<p>
+ * 3. Selects the option Extensions<p>
+ * 4. Selects the option Import/Export Data<p>
+ * 5. Press the "Export from a text file" option to open the feature<p>
+ * 6. A window named "Export from a text file" should appear with data to be filled by the user<p>
+ * 7. Writes the name of the exported text file<p>
+ * 8. On the label "Special character" the user writes in the textbox located at the right side of the label, the character which will be used to separate columns<p>
+ * 9. Press the "Export" button to import the data<p>
+ * 10. The exported text file should have the data from the cells selected by the user<p>
+ * 11. No error should occur<p>
+ * 
  * <p>
  *
  * <b>Design</b><p>
