2aedff842b8df8fac4b1b96a071026c3a7c68fca
Marcos Dourado <1140956@isep.ipp.pt>
2017-06-01 17:41:38 +0100

    LAPR4E17DL-57 LAPR4E17DL-137 LAPR4E17DL-136 #comment analysis and design done #resolve

---
+++ src/main/java/lapr4/blue/s1/lang/n1140956/ConditionalFormating/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * Technical documentation regarding the user story Lang03.1: Conditional Formatting of Cells.
+ * <p>
+ * 
+ * <b>Attention: This feature increment and this documentation are work in progress! </b><p>
+ *
+ * <b>Requirement</b><p>
+ * Update the "style" extension so that it can be used for the conditional formatting of cells based on the
+result of the execution of formulas. For the style of a cell to be conditional it must have an associated
+formula and two formatting styles. One of the styles is applied when the formula evaluates to true and
+the other when it evaluates to false. The editing of these settings should be done in a sidebar window.
+
+ * <b>Attention:</b><p> 
+ * 
+ * <b>Analysis</b><p>
+ * This tool is located on "style" extension. After opening the "style" extension we choose the "Conditional Formating" option
+ * When the tool is running the user needs to provide the expression he wants and the associated two formatting styles.
+ * In the end, one of the styles will be applied.
+ * 
+ * <b>Notes</b><p>
+ *
+ * <b>storeContent</b><p>
+ *
+ * <b>Tests</b><p>
+ * 
+ * <b>Design</b><p>
+ *
+ * <b>Code</b><p>
+ * 
+ * @author Marcos Dourado (1140956)
+ */
+package lapr4.blue.s1.lang.n1140956.ConditionalFormating;
