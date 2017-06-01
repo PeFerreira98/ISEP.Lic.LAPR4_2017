16212cd319d9d0eddcfdcd80a1037fb094e48f24
NunoFilipe [1131106] <1131106@isep.ipp.pt>
2017-06-01 15:06:49 +0100

    LAPR4E17DL-101 #comment start the implemetation

---
+++ src/main/java/lapr4/red/s1/core/n1131106/extensionsmanager/application/ExtensionNavigatorController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr4.red.s1.core.n1131106.extensionsmanager.application;
+
+import csheets.ui.ctrl.UIController;
+
+/**
+ *
+ * @author Nuno Filipe 1131106
+ */
+public class ExtensionNavigatorController {
+
+    private UIController uiController;
+
+    public ExtensionNavigatorController(UIController uiController) {
+        this.uiController = uiController;
+    }
+
+}
