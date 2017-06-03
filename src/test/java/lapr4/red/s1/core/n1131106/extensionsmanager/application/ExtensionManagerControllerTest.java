1b074ec7e32a2ace0fe01539db388fda36aef1df
NunoFilipe [1131106] <1131106@isep.ipp.pt>
2017-06-03 14:58:14 +0100

    LAPR4E17DL-102 #comment testChangeState done

--- src/test/java/lapr4/red/s1/core/n1131106/extensionsmanager/application/ExtensionManagerControllerTest.java
+++ src/test/java/lapr4/red/s1/core/n1131106/extensionsmanager/application/ExtensionManagerControllerTest.java
@@ -5,6 +5,10 @@
  */
 package lapr4.red.s1.core.n1131106.extensionsmanager.application;
 
+import csheets.CleanSheets;
+import csheets.ui.ctrl.UIController;
+import javax.swing.JComponent;
+import javax.swing.JToolBar;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,22 +21,22 @@ import static org.junit.Assert.*;
  * @author Nuno Filipe 1131106
  */
 public class ExtensionManagerControllerTest {
-    
+
     public ExtensionManagerControllerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -42,13 +46,46 @@ public class ExtensionManagerControllerTest {
      */
     @Test
     public void testChangeState() {
-//        System.out.println("changeState");
-//        int i = 0;
-//        boolean flag = false;
-//        ExtensionManagerController instance = null;
-//        instance.changeState(i, flag);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+        
+        System.out.println("changeState");
+        int i = 0;
+        boolean flag = false;
+        UIController uiController = new UIController(new CleanSheets());
+        ExtensionManagerController instance = new ExtensionManagerController(uiController);
+        instance.changeState(i, flag);
+        boolean result = false;
+
+        if (uiController.getExtensions()[i].getSideBar() != null) {
+            JComponent sidebar = uiController.getExtensions()[i].getSideBar();
+            for (int j = 0; j < sidebar.getComponents().length; j++) {
+                if (sidebar.getComponents()[j].isEnabled() || sidebar.getComponents()[j].isVisible()) {
+                    result = true;
+                }
+            }
+        }
+        if (uiController.getExtensions()[i].getCellDecorator() != null) {
+            if (uiController.getExtensions()[i].getCellDecorator().isEnabled()) {
+                result = true;
+            }
+        }
+
+        if (uiController.getExtensions()[i].getMenu() != null) {
+            if (uiController.getExtensions()[i].getMenu().isEnabled()) {
+                result = true;
+            }
+        }
+
+        if (uiController.getExtensions()[i].getToolBar() != null) {
+            JToolBar toolbar = uiController.getExtensions()[i].getToolBar();
+            for (int j = 0; j < toolbar.getComponents().length; j++) {
+                if (toolbar.getComponents()[j].isEnabled() || toolbar.getComponents()[j].isVisible()) {
+                    result = true;
+                }
+
+            }
+        }
+
+        assertEquals(flag, result);
     }
-    
+
 }
