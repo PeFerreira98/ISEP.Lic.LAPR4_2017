6616dd9e5c165d97634c401387a5c726c4aa75b1
celsoeusebio <1140376@isep.ipp.pt>
2017-06-05 00:52:35 +0100

    #LAPR4E17DL-7 Core02.2 Last touches in the implementation, adding package info

---
+++ src/main/java/lapr4/red/s1/core/n1140376/comments/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Technical documentation regarding the user story Core02.2: Tooltip and User Associated to Comment.
+ * 
+ * <h3>4.2. UC Realization</h3>
+ * To realize this user story we will need to create a new comment classe, and a new panel that will cotain various commentPanels called CommentsPanel
+ * <p>
+ * <b>Note:</b> It is very important that in the final version of this technical documentation the elements depicted in these design diagrams exist in the code!
+ * 
+ * <h3>UC Realization Diagram</h3>
+ * The following diagram shows what the user does in this Use Case
+ * <p>
+ * <img src="comments_extension_uc_realization2.png" alt="image">
+ * 
+ *
+ * <h3>Design</h3>
+ * The following diagram illustrates what happens in more detail in this Use Case
+ * <p>
+ * <img src="core02_01_design2.png" alt="image"> 
+ * @author 1140376
+ */
+package lapr4.red.s1.core.n1140376.comments;
+
---
+++ src/main/java/lapr4/red/s1/core/n1140376/comments/ui/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the UI of an extension to support several comments on a cell and tooltip display.
+ * 
+ * @author 1140376
+ * 
+ */
+package lapr4.red.s1.core.n1140376.comments.ui;
