a8774223e59dcb1cc0ca597da4461798325932f2
1970581 <1970581@isep.ipp.pt>
2017-06-06 23:59:18 +0100

    #LAPR4E17DL-158 #time 3h #comment Core01.2 Analisys start

---
+++ src/main/java/lapr4/green/s2/core/n1970581/autodescriptionextensions/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * Technical documentation regarding the user story IPC02.1: Find Workbooks Team Green Sprint 1
+ * 
+ * <p>
+ * <b>-Note: this is not a template/example but a real thing.</b>
+ * <p>
+ * <b>Scrum Master: -(yes/no)- no</b>
+ * 
+ * <p>
+ * <b>Area Leader: -(yes/no)- yes</b>
+ * 
+ * <h2>1. Notes</h2>
+ * 
+ * -Notes about the week's work.-
+ * <p>
+ * 
+ * Work notes/log: S2 6-05-2017 (terca) <br>
+ * During the morning we had a meeting with the client. <br>
+ * In the afternoon we has scrumm meeting, where each chose a use case. <br>
+ * Analisys initiated.
+ * 
+ * @author Hugo Bento 1970581
+ */
+package lapr4.green.s2.core.n1970581.autodescriptionextensions;
