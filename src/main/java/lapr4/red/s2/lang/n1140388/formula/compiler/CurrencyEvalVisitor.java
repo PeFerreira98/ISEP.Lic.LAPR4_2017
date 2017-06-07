6201ddd21070224f1fae704dce7adfa39e6713fe
Alexandra Ferreira 1140388@isep.ipp.pt <1140388@isep.ipp.pt>
2017-06-07 22:21:06 +0100

    LAPR4E17DL-149 #time 3h Changed variables of euro and pound and add javadoc to Money Rate; Implement the visit on CurrencyEvalVisitor methods

--- src/main/java/lapr4/red/s2/lang/n1140388/formula/compiler/CurrencyEvalVisitor.java
+++ src/main/java/lapr4/red/s2/lang/n1140388/formula/compiler/CurrencyEvalVisitor.java
@@ -6,7 +6,15 @@
 package lapr4.red.s2.lang.n1140388.formula.compiler;
 
 import csheets.core.Cell;
+import csheets.core.formula.BinaryOperation;
+import csheets.core.formula.BinaryOperator;
 import csheets.core.formula.Expression;
+import csheets.core.formula.Literal;
+import csheets.core.formula.compiler.FormulaCompilationException;
+import csheets.core.formula.lang.CellReference;
+import java.math.BigDecimal;
+import lapr4.blue.s1.lang.n1141233.formula.lang.Language;
+import lapr4.red.s2.lang.n1140388.formula.lang.MoneyRate;
 
 /**
  *
@@ -17,6 +25,7 @@ public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
     private Cell cell = null;
     int numberOfErros;
     private final StringBuilder errorBuffer;
+    private String currencyName;
 
     public CurrencyEvalVisitor(Cell cell) {
         this.cell = cell;
@@ -34,39 +43,65 @@ public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
 
     @Override
     public Expression visitExpression(CurrencyParser.ExpressionContext ctx) {
-        return visit(ctx.getChild(1));
+        //visitCurrenciesName
+        visit(ctx.getChild(1));
+
+        //visitCurrenciesCount
+        return visit(ctx.getChild(3));
     }
 
     @Override
-    public Expression visitCurrenciesName(CurrencyParser.CurrenciesNameContext ctx){
-        return null;
-    }
-    
-    @Override
-    public Expression visitCurrenciesSymb(CurrencyParser.CurrenciesSymbContext ctx){
+    public Expression visitCurrenciesName(CurrencyParser.CurrenciesNameContext ctx) {
+        currencyName = ctx.getText();
+
         return null;
     }
 
     @Override
-    public Expression visitCurrenciesCount(CurrencyParser.CurrenciesCountContext ctx){
-        return null;
+    public Expression visitCurrenciesCount(CurrencyParser.CurrenciesCountContext ctx) {
+        try {
+            if (ctx.getChildCount() == 3) {
+
+                // Convert binary operation
+                BinaryOperator operator = Language.getInstance().getBinaryOperator(ctx.getChild(1).getText());
+                return new BinaryOperation(visit(ctx.getChild(0)),
+                        operator,
+                        visit(ctx.getChild(2))
+                );
+            }
+
+        } catch (FormulaCompilationException ex) {
+            addVisitError(ex.getMessage());
+        }
+
+        return visitChildren(ctx);
     }
-    
+
     @Override
-    public Expression visitArithmetic_basic(CurrencyParser.Arithmetic_basicContext ctx){
-        return null;
+    public Expression visitMoney(CurrencyParser.MoneyContext ctx) {
+
+        String number = ctx.getChild(0).getText();
+        String currency = ctx.getChild(1).getText();
+        MoneyRate mr = new MoneyRate();
+
+        return new Literal(mr.calculateValue(currencyName, number, currency));
     }
-    
+
     @Override
-    public Expression visitArithmetic_pro(CurrencyParser.Arithmetic_proContext ctx){
+    public Expression visitArithmetic_basic(CurrencyParser.Arithmetic_basicContext ctx) {
         return null;
     }
-    
+
     @Override
-    public Expression visitMoney(CurrencyParser.MoneyContext ctx){
+    public Expression visitArithmetic_pro(CurrencyParser.Arithmetic_proContext ctx) {
         return null;
     }
-            
+
+    @Override
+    public Expression visitCurrenciesSymb(CurrencyParser.CurrenciesSymbContext ctx) {
+        return visitChildren(ctx);
+    }
+
     private void addVisitError(String msg) {
         errorBuffer.append(msg).append("\n");
         numberOfErros++;
--- src/main/java/lapr4/red/s2/lang/n1140388/formula/lang/MoneyRate.java
+++ src/main/java/lapr4/red/s2/lang/n1140388/formula/lang/MoneyRate.java
@@ -5,8 +5,12 @@
  */
 package lapr4.red.s2.lang.n1140388.formula.lang;
 
+import com.ibm.icu.text.NumberFormat;
+import com.ibm.icu.util.ULocale;
 import csheets.core.Value;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Locale;
 
 /**
  *
@@ -23,9 +27,9 @@ public class MoneyRate {
     private static BigDecimal excPoundToDollar = new BigDecimal(1.29072);
     private static BigDecimal excPoundToEuro = new BigDecimal(1.14587);
 
-    private static final String EURO_SYMB = "€";
+    private static final String EURO_SYMB = "\u20AC";
     private static final String DOLLAR_SYMB = "$";
-    private static final String POUND_SYMB = "£";
+    private static final String POUND_SYMB = "\u00A3";
 
     private static final String EURO_NAME = "euro";
     private static final String DOLLAR_NAME = "dollar";
@@ -34,26 +38,56 @@ public class MoneyRate {
     public MoneyRate() {
     }
 
+    /**
+     * Returns the value of the exchange dollar to euro.
+     *
+     * @return value of the exchange dollar to euro.
+     */
     public static BigDecimal excDollarToEuro() {
         return excDollarToEuro;
     }
 
+    /**
+     * Returns the value of the exchange dollar to pound.
+     *
+     * @return value of the exchange dollar to pound.
+     */
     public static BigDecimal excDollarToPound() {
         return excDollarToPound;
     }
 
+    /**
+     * Returns the value of the exchange euro to dollar.
+     *
+     * @return value of the exchange euro to dollar.
+     */
     public static BigDecimal excEuroToDollar() {
         return excEuroToDollar;
     }
 
+    /**
+     * Returns the value of the exchange euro to pound.
+     *
+     * @return value of the exchange euro to pound.
+     */
     public static BigDecimal excEuroToPound() {
         return excEuroToPound;
     }
 
+    /**
+     * Returns the value of the exchange pound to dollar.
+     *
+     * @return value of the exchange pound to dollar.
+     */
     public static BigDecimal excPoundToDollar() {
         return excPoundToDollar;
     }
 
+    /**
+     * Returns the value of the exchange pound to euro.
+     *
+     * @return value of the exchange pound to euro.
+     */
     public static BigDecimal excPoundToEuro() {
         return excPoundToEuro;
     }
@@ -62,46 +96,46 @@ public class MoneyRate {
         if (value.contains(",")) {
             value = value.replace(',', '.');
         }
-        
+
         BigDecimal result = new BigDecimal(value);
 
         if (currencyName.contains(EURO_NAME)) {
             if (currencySymbol.contains(EURO_SYMB)) {
                 return new Value(result);
             }
-            
+
             if (currencySymbol.contains(DOLLAR_SYMB)) {
                 return new Value(result.multiply(excDollarToEuro()));
             }
-            
+
             if (currencySymbol.contains(POUND_SYMB)) {
                 return new Value(result.multiply(excPoundToEuro()));
             }
-            
+
         } else if (currencyName.contains(DOLLAR_NAME)) {
 
             if (currencySymbol.contains(EURO_SYMB)) {
                 return new Value(result.multiply(excEuroToDollar()));
             }
-            
+
             if (currencySymbol.contains(DOLLAR_SYMB)) {
                 return new Value(result);
             }
-            
+
             if (currencySymbol.contains(POUND_SYMB)) {
                 return new Value(result.multiply(excPoundToDollar()));
             }
-            
+
         } else if (currencyName.contains(POUND_NAME)) {
-            
+
             if (currencySymbol.contains(EURO_SYMB)) {
                 return new Value(result.multiply(excEuroToPound()));
             }
-            
+
             if (currencySymbol.contains(DOLLAR_SYMB)) {
                 return new Value(result.multiply(excDollarToPound()));
             }
-            
+
             if (currencySymbol.contains(POUND_SYMB)) {
                 return new Value(result);
             }
