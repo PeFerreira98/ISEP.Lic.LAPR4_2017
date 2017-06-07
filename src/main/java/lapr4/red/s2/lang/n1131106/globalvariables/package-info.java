58a7343131e6bdc22a8ff08c72e6b7445a046220
NunoFilipe [1131106] <1131106@isep.ipp.pt>
2017-06-07 14:32:26 +0100

    LAPR4E17DL-150 #comment Start the analysis

---
+++ src/main/java/lapr4/red/s2/lang/n1131106/globalvariables/package-info.java
@@ -0,0 +1,70 @@
+/**
+ * Technical documentation regarding the user story Lang02.2: Global Variables.
+ * 
+ * <p>
+ * <b>Scrum Master: -(yes/no)- no</b>
+ * 
+ * <p>
+ * <b>Area Leader: -(yes/no)- no</b>
+ * 
+ * <h2>1. Notes</h2>
+ * 
+ * -Notes about the week's work.-
+ * 
+ * <p>
+ * Issue in Jira:
+ * <a href="https://jira.dei.isep.ipp.pt:8443/browse/LAPR4E17DL-55">Lang02.2- Global Variables</a>
+ *
+ * 
+ * <h2>2. Requirement</h2>
+ * The global variables that exist in the context of the workbook, are persisted with the workbook and their names must start with the "@" sign.
+ * At its creation, the global variable must be reffered in a formula.The value must be set with the following assign operator ":=".
+ * 
+ * <h2>3. Analysis</h2>
+ * 
+ * Analyze how to associate the extensions to a window, which can be enabled and disabled.
+ * How do we enable and disable an extension.
+ * This window will also be an extension, it will be a SideBar on the View menu that can be enabled and disabled.
+ * This extension will serve as manager of all others.
+ * The implementation will pass by creating a subclass of abstract class Extension.
+ * 
+ * <img src="lang02_02_analysis.png" alt="image"> 
+ * 
+ * <h2>4. Design</h2>
+ * <p>
+ * 
+ * 
+ *
+ * <h3>4.1. Functional Tests</h3>
+ * 
+ * 
+ * <h3>4.2. Classes</h3>
+ *  <img src="" alt="image"> 
+ * 
+ * 
+ * <h2>8. Work Log</h2> 
+ * 
+ * <p>
+ * <b>Monday:</b>
+ * </p>
+ * <p>
+ * 1. Apresentation the Sprint1
+ * </p>
+ * 
+ * <p>
+ * <b>Tuesday:</b>
+ * </p>
+ * <p>
+ * 1. Start the analysis of my UC
+ * </p>
+ * <p>
+ * <u>Blocking:</u>
+ * </p>
+ * <p>
+ * 1. -nothing-
+ * </p>
+ * 
+ * 
+ * @author Nuno Costa 1131106
+ */
+package lapr4.red.s2.lang.n1131106.globalvariables;
