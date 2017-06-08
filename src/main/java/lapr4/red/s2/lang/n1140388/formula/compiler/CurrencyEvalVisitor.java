1743e92bbb8bff93dbfab78c6095b91a2d7cdc78
Alexandra Ferreira 1140388@isep.ipp.pt <1140388@isep.ipp.pt>
2017-06-08 18:01:11 +0100

    LAPR4E17DL-149 changed the package of MoneyRateUI, add javadoc

--- src/main/java/lapr4/red/s2/lang/n1140388/formula/compiler/CurrencyEvalVisitor.java
+++ src/main/java/lapr4/red/s2/lang/n1140388/formula/compiler/CurrencyEvalVisitor.java
@@ -6,15 +6,11 @@
 package lapr4.red.s2.lang.n1140388.formula.compiler;
 
 import csheets.core.Cell;
-import csheets.core.Value;
 import csheets.core.formula.BinaryOperation;
 import csheets.core.formula.BinaryOperator;
 import csheets.core.formula.Expression;
 import csheets.core.formula.Literal;
-import csheets.core.formula.UnaryOperation;
 import csheets.core.formula.compiler.FormulaCompilationException;
-import csheets.core.formula.lang.CellReference;
-import java.math.BigDecimal;
 import lapr4.blue.s1.lang.n1141233.formula.lang.Language;
 import lapr4.red.s2.lang.n1140388.formula.lang.MoneyRate;
 
@@ -24,25 +20,44 @@ import lapr4.red.s2.lang.n1140388.formula.lang.MoneyRate;
  */
 public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
 
-    private Cell cell = null;
     int numberOfErros;
     private final StringBuilder errorBuffer;
     private String currencyName;
 
+    /**
+     * Creates the currency eval visitor.
+     * 
+     * @param cell the cell of the input
+     */
     public CurrencyEvalVisitor(Cell cell) {
-        this.cell = cell;
         numberOfErros = 0;
         errorBuffer = new StringBuilder();
     }
 
+    /**
+     * Return the number of errors.
+     * 
+     * @return the number of errors.
+     */
     public int getNumberOfErrors() {
         return numberOfErros;
     }
 
+    /**
+     * Return the message of the error.
+     * 
+     * @return the message of the error.
+     */
     public String getErrorsMessage() {
         return errorBuffer.toString();
     }
 
+    /**
+     * Visit the expression.
+     * 
+     * @param ctx the expression
+     * @return the value of the expression
+     */
     @Override
     public Expression visitExpression(CurrencyParser.ExpressionContext ctx) {
         //visitCurrenciesName
@@ -52,6 +67,12 @@ public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
         return visit(ctx.getChild(3));
     }
 
+    /**
+     * Return the currencie name.
+     * 
+     * @param ctx the expression
+     * @return the currencie name
+     */
     @Override
     public Expression visitCurrenciesName(CurrencyParser.CurrenciesNameContext ctx) {
         currencyName = ctx.getText();
@@ -59,6 +80,12 @@ public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
         return null;
     }
 
+    /**
+     * Return the count of the expression.
+     * 
+     * @param ctx the expression
+     * @return the value of the expression
+     */
     @Override
     public Expression visitCurrenciesCount(CurrencyParser.CurrenciesCountContext ctx) {
         try {
@@ -76,20 +103,19 @@ public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
 
                 if (operator.equals(Language.getInstance().getBinaryOperator("*"))
                         || operator.equals(Language.getInstance().getBinaryOperator("/"))) {
-                    
+
                     MoneyRate mr = new MoneyRate();
 
-                    if(ctx.getChild(0).getChildCount() == 0){
+                    if (ctx.getChild(0).getChildCount() == 0) {
                         return new BinaryOperation(new Literal(mr.calculateValueWithoutCurrency(currencyName, ctx.getChild(0).getText())),
-                            operator,
-                            visit(ctx.getChild(2)));
-                    }else{
+                                operator,
+                                visit(ctx.getChild(2)));
+                    } else {
                         return new BinaryOperation(visit(ctx.getChild(0)),
-                            operator,
-                            new Literal(mr.calculateValueWithoutCurrency(currencyName, ctx.getChild(2).getText())));
+                                operator,
+                                new Literal(mr.calculateValueWithoutCurrency(currencyName, ctx.getChild(2).getText())));
                     }
-                    
-                    
+
                 }
             }
         } catch (FormulaCompilationException ex) {
@@ -99,21 +125,38 @@ public class CurrencyEvalVisitor extends CurrencyBaseVisitor<Expression> {
         return visitChildren(ctx);
     }
 
+    /**
+     * Return the exchange rate of money.
+     * 
+     * @param ctx the expression
+     * @return the exchange
+     */
     @Override
     public Expression visitMoney(CurrencyParser.MoneyContext ctx) {
 
         String number = ctx.getChild(0).getText();
         MoneyRate mr = new MoneyRate();
         String currency = ctx.getChild(1).getText();
-        
+
         return new Literal(mr.calculateValue(currencyName, number, currency));
     }
 
+    /**
+     * Return the currency.
+     * 
+     * @param ctx the expression
+     * @return the currency
+     */
     @Override
     public Expression visitCurrenciesSymb(CurrencyParser.CurrenciesSymbContext ctx) {
         return visitChildren(ctx);
     }
 
+    /**
+     * Add a visit error.
+     * 
+     * @param msg the message of error
+     */
     private void addVisitError(String msg) {
         errorBuffer.append(msg).append("\n");
         numberOfErros++;
