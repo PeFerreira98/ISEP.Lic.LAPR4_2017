2631909cf9227f17f3b148dda709c8c6774f1cc2
Marcos Dourado <1140956@isep.ipp.pt>
2017-06-08 20:30:11 +0100

    LAPR4E17DL-88 LAPR4E17DL-186 #time 4h #comment analysis for user settings

---
+++ src/main/java/lapr4/blue/s2/ipc/n1140956/ChatApplication/package-info.java
@@ -0,0 +1,38 @@
+/**
+ * Technical documentation regarding the user story Lang03.1: Conditional Formatting of Cells.
+ * <p>
+ * 
+ * <b>Attention: This feature increment and this documentation are work in progress! </b><p>
+ *
+ * <b>Requirement</b><p>
+ * Cleansheets should now use the user name of the system as the basis for the user profile of the chat.
+The end user should be able to add an icon or a photo to its profile as well as a nickname. Each user
+should have have a status (i.e., online or offline). Cleansheets should automatically discover all users in
+the local network. The sidebar window should now have the conversations organized by user. The window
+should also display the status of the users and their icon and nickname. When a user state is offline it will
+not receive any messages from other instances of Cleansheets. Profile configuration and message history
+should be persistent.
+
+ * <b>Attention:</b><p> 
+ * 
+ * <b>Analysis</b><p>
+ * 
+ * <img src="ipc05.2_analysis.png" alt="image"> 
+ * 
+ * <b>Notes</b><p>
+ *
+ * <b>storeContent</b><p>
+ *
+ * <b>Tests</b><p>
+ * <b>Test1:</b> 
+ * 
+ * <b>Design</b><p>
+ * 
+ * <p>
+ * 
+ * <b>Code</b><p>
+ * 
+ * 
+ * @author Marcos Dourado (1140956)
+ */
+package lapr4.blue.s2.ipc.n1140956.ChatApplication;
