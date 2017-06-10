596b706d0e2e9886cf01906f8f1376fcb419021d
Rafael Vieira <1141233@isep.ipp.pt>
2017-06-10 13:36:18 +0100

    #LAPR4E17DL-85 #LAPR4E17DL-166 #comment analysis concluded

---
+++ src/main/java/lapr4/blue/s2/ipc/n1141233/importexportlink/controller/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides controllers for the user interface.
+ *
+ * @author Rafael Vieira
+ */
+package lapr4.blue.s2.ipc.n1141233.importexportlink.controller;
---
+++ src/main/java/lapr4/blue/s2/ipc/n1141233/importexportlink/ui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the UI and actions for the import/export data (text) extension.
+ *
+ * @author Rafael Vieira
+ */
+package lapr4.blue.s2.ipc.n1141233.importexportlink.ui;
+
+
