d3cf9bb842075a441a5b55ea0b8dba7e278021aa
Marcos Dourado <1140956@isep.ipp.pt>
2017-06-12 00:04:52 +0100

    LAPR4E17DL-88 LAPR4E17DL-187 #time 3h #comment design done for user settings and getting users

--- src/main/java/lapr4/blue/s2/ipc/n1140956/ChatApplication/package-info.java
+++ src/main/java/lapr4/blue/s2/ipc/n1140956/ChatApplication/package-info.java
@@ -12,10 +12,9 @@ the local network. The sidebar window should now have the conversations organize
 should also display the status of the users and their icon and nickname. When a user state is offline it will
 not receive any messages from other instances of Cleansheets. Profile configuration and message history
 should be persistent.
-
- * <b>Attention:</b><p> 
  * 
  * <b>Analysis</b><p>
+ * On the sidebar window there is a chat where we can chat with other persons who is online. There's an setting's option too where the user can change their nickname and perfil photo.
  * 
  * <img src="ipc05.2_analysis.png" alt="image"> 
  * 
@@ -24,14 +23,30 @@ should be persistent.
  * <b>storeContent</b><p>
  *
  * <b>Tests</b><p>
- * <b>Test1:</b> 
+ * <b>Test1:</b>ConversationStorageTest<p> >
+ * See Package lapr4.blue.s2.ipc.n1140956.ChatApplication:<p>
+ * ConversationStorageTest<p>
+ * <b>Test1:</b>ChatUsersStorageTest<p> >
+ * See Package lapr4.blue.s2.ipc.n1140956.ChatApplication:<p>
+ * ChatUsersStorageTest<p>
  * 
- * <b>Design</b><p>
+ * <b>Design</b><p> * 
+ *<img src="ipc05.2_design(getUsers).png" alt="image"> 
+ *<img src="ipc05.2_design(userSettings).png" alt="image"> 
  * 
  * <p>
  * 
  * <b>Code</b><p>
+ * The following classes and interfaces implement this use case.<p>
+ * Package lapr4.blue.s2.ipc.n1140956.ChatApplication:<p>
+ * {@link lapr4.blue.s2.ipc.n1140956.ChatApplication.ChatUser}
+ * {@link lapr4.blue.s2.ipc.n1140956.ChatApplication.ChatUserStorage}
+ * {@link lapr4.blue.s2.ipc.n1140956.ChatApplication.Conversation}
+ * {@link lapr4.blue.s2.ipc.n1140956.ChatApplication.ConversationStorage}
  * 
+ * <p>
+ * Package @link lapr4.blue.s2.ipc.n1140956.ChatApplication.ui:<p>
+ * {@link @link lapr4.blue.s2.ipc.n1140956.ChatApplication.ui.ChatUserSettingsUI}
  * 
  * @author Marcos Dourado (1140956)
  */
