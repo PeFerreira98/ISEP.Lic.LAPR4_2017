a034acf69e439949e757eae0e1e0525390af6892
Alexandra Ferreira 1140388@isep.ipp.pt <1140388@isep.ipp.pt>
2017-06-12 22:20:54 +0100

    LAPR4E17DL-193 #time 40min Add chat rooms to the Menu, Add a ChatRoom class and the type of ChatRoom (private or public) Add an extension to the use case

---
+++ src/main/java/lapr4/red/s3/ipc/n1140388/chatrooms/ui/CreateRoomAction.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr4.red.s3.ipc.n1140388.chatrooms.ui;
+
+import csheets.ui.ctrl.BaseAction;
+import csheets.ui.ctrl.UIController;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author Alexandra Ferreira 1140388
+ */
+public class CreateRoomAction extends BaseAction {
+
+    /**
+     * User Interface Controller
+     */
+    UIController uiController;
+
+
+    public CreateRoomAction(UIController uiController) {
+        this.uiController = uiController;
+    }
+
+    @Override
+    protected String getName() {
+        return "Create a Chat Room";
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
