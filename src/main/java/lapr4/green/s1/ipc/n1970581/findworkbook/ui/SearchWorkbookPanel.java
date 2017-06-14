e5accf10e89689080ce209c2d2c76a843dfd20a8
NunoFilipe [1131106] <1131106@isep.ipp.pt>
2017-06-14 15:46:39 +0100

    LAPR4E17DL-196 #comment start the implementation, create RealTimeSearchUI

--- src/main/java/lapr4/green/s1/ipc/n1970581/findworkbook/ui/SearchWorkbookPanel.java
+++ src/main/java/lapr4/green/s1/ipc/n1970581/findworkbook/ui/SearchWorkbookPanel.java
@@ -18,42 +18,57 @@ import lapr4.blue.s2.ipc.n1140948.advancedworkbooksearch.UI.PreviewUI;
 import lapr4.green.s1.ipc.n1970581.findworkbook.FileDTO;
 import lapr4.green.s1.ipc.n1970581.findworkbook.SearchWorkbookExtension;
 import lapr4.green.s1.ipc.n1970581.findworkbook.controller.SearchWorkbookController;
+import lapr4.red.s3.ipc.n1131106.realtimeworkbook.ui.RealTimeSearchUI;
 
 /**
  * Sidebar Pannel for use case: IPC01.1 Search Workbook
+ *
  * @author Hugo
  */
 public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer {
 
-    /** The ui Controller of the main window */
+    /**
+     * The ui Controller of the main window
+     */
     private final UIController uiController;
-    
-    /** The Controller of the use case */
+
+    /**
+     * The Controller of the use case
+     */
     private final SearchWorkbookController controller;
-    
-    /** file chooser object */
+
+    /**
+     * file chooser object
+     */
     private JFileChooser chooser;
-    
+
     private final static String CHOOSER_TITLE = "Choose the search root directory";
-    
-    /** chosen root Directory*/
+
+    /**
+     * chosen root Directory
+     */
     private File rootDir;
-    
-    /** root directory path */
+
+    /**
+     * root directory path
+     */
     private String rootDirPath;
-    
-    /** The defaultListModel */
+
+    /**
+     * The defaultListModel
+     */
     private final DefaultListModel<FileDTO> defaultListModel;
-    
+
     /**
      * Creates new form SearchWorkbookPanel
+     *
      * @param uiController the uiController
      */
     public SearchWorkbookPanel(UIController uiController) {
         this.uiController = uiController;
         this.controller = new SearchWorkbookController(uiController);
         initComponents();
-        this.defaultListModel = new DefaultListModel<>();        
+        this.defaultListModel = new DefaultListModel<>();
     }
 
     /**
@@ -72,6 +87,7 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
         jScrollPane1 = new javax.swing.JScrollPane();
         jListWorkbooks = new javax.swing.JList();
         PreviewWorkbookButton = new javax.swing.JButton();
+        realTimeButton = new javax.swing.JButton();
 
         setBorder(javax.swing.BorderFactory.createTitledBorder(null, SearchWorkbookExtension.NAME, javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP));
         setName(SearchWorkbookExtension.NAME);
@@ -118,6 +134,13 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
             }
         });
 
+        realTimeButton.setText("RealTime");
+        realTimeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                realTimeButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -127,14 +150,15 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(PreviewWorkbookButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButtonOpen, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addComponent(jButtonOpen, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(realTimeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButtonRoot)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextFieldRoot, javax.swing.GroupLayout.DEFAULT_SIZE, 215, Short.MAX_VALUE)
+                        .addComponent(jTextFieldRoot)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButtonSearch)
                         .addContainerGap())))
@@ -150,6 +174,8 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(realTimeButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(PreviewWorkbookButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButtonOpen))
@@ -161,7 +187,7 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
 
     private void jButtonRootActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonRootActionPerformed
         // TODO add your handling code here:
-        chooser = new JFileChooser(); 
+        chooser = new JFileChooser();
         chooser.setCurrentDirectory(new java.io.File("."));
         //chooser.setCurrentDirectory(new java.io.File("d:/testc/"));
         chooser.setDialogTitle(CHOOSER_TITLE);
@@ -169,7 +195,7 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
         chooser.showOpenDialog(this);
         File dir = chooser.getSelectedFile();
         //System.out.println(file.toPath());
-        if(!dir.isDirectory()){
+        if (!dir.isDirectory()) {
             Logger.getGlobal().log(Level.SEVERE, "Directory chooser chose a non directory.");
             return;
         }
@@ -189,26 +215,36 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
     private void jListWorkbooksMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jListWorkbooksMouseClicked
         // TODO add your handling code here:
         //System.out.println("mouse clicked!!!");
-        boolean doubleClick = (evt.getClickCount() ==2);
+        boolean doubleClick = (evt.getClickCount() == 2);
         if (doubleClick) {
             //System.out.println("double");
             openWorkbook();
-            
+
         }
-        
+
     }//GEN-LAST:event_jListWorkbooksMouseClicked
 
     private void jButtonOpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonOpenActionPerformed
         // TODO add your handling code here:
-        if (this.jListWorkbooks.getSelectedValue() != null) openWorkbook();
+        if (this.jListWorkbooks.getSelectedValue() != null) {
+            openWorkbook();
+        }
     }//GEN-LAST:event_jButtonOpenActionPerformed
 
     private void PreviewWorkbookButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PreviewWorkbookButtonActionPerformed
         // TODO add your handling code here:        
         FileDTO file = (FileDTO) jListWorkbooks.getSelectedValue();
-        
+
         PreviewUI previewUI = new PreviewUI(file, uiController);
     }//GEN-LAST:event_PreviewWorkbookButtonActionPerformed
+
+    private void realTimeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_realTimeButtonActionPerformed
+        // TODO add your handling code here:
+
+       // RealTimeSearchUI realTimeSearch = new RealTimeSearchUI(new javax.swing.JFrame(), true, controller, SearchWorkbookPanel.this);
+      //  realTimeSearch.setVisible(true);
+
+    }//GEN-LAST:event_realTimeButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -219,11 +255,14 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
     private javax.swing.JList jListWorkbooks;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextFieldRoot;
+    private javax.swing.JButton realTimeButton;
     // End of variables declaration//GEN-END:variables
-    
+
     @Override
-    public synchronized void  update(Observable o, Object arg) {
-        if( !(arg instanceof FileDTO) ) return;
+    public synchronized void update(Observable o, Object arg) {
+        if (!(arg instanceof FileDTO)) {
+            return;
+        }
         FileDTO fileDTO = (FileDTO) arg;
         //System.out.println(fileDTO.toString());
         //Logger.getGlobal().log(Level.SEVERE, fileDTO.toString());
@@ -233,29 +272,28 @@ public class SearchWorkbookPanel extends javax.swing.JPanel implements Observer 
         this.jButtonOpen.setEnabled(true);
         this.PreviewWorkbookButton.setEnabled(true);
     }
-    
+
     /**
      * CUSTOM: Calls the controller to open the workbook.
      */
-    public void openWorkbook(){
+    public void openWorkbook() {
         int index = this.jListWorkbooks.getSelectedIndex();
-        Object obj =  this.jListWorkbooks.getSelectedValue();
-        if(obj == null) {
+        Object obj = this.jListWorkbooks.getSelectedValue();
+        if (obj == null) {
             //throw new NullPointerException("null returned from this.jListWorkbooks.getSelectedValue() while opening woorkbook");
-            JOptionPane.showMessageDialog(this, "Null workbook selected from list", "Error" , JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Null workbook selected from list", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
         FileDTO fileDTO = (FileDTO) this.jListWorkbooks.getSelectedValue();
-        
+
         /* Not needed, when it opens, it doesn't discard the other open workbooks.
         boolean modified = true;
         modified = this.controller.isActiveWorkbookModified();
         int answer = 0;  // NO = 1 , YES = 0
         if(modified) answer = JOptionPane.showConfirmDialog(this.myselfPanel, "Unsaved Modifications Detected. Still wish to open?", "Title" , JOptionPane.YES_NO_OPTION);
         System.out.println("Anser:" + answer);
-        */
-        
+         */
         this.controller.openWorkbook(fileDTO);
     }
-    
+
 }
