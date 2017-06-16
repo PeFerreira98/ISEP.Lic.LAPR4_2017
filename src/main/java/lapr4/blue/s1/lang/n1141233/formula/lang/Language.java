025729226fa9714eb2b288a068996fcc4e8b8e41
Tiago Carvalho <1140618@isep.ipp.pt>
2017-06-16 22:00:26 +0100

    #LAPR4E17DL-200 #comment implementation of operators, incompleted #time 2h

--- src/main/java/lapr4/blue/s1/lang/n1141233/formula/lang/Language.java
+++ src/main/java/lapr4/blue/s1/lang/n1141233/formula/lang/Language.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import lapr4.gray.s1.lang.n3456789.formula.NaryOperator;
 import lapr4.blue.s1.lang.n1141233.formula.ForOperator;
+import lapr4.green.s3.lang.n1140618.WhileDoOperator;
 
 /**
  * A factory for creating certain types of language elements.
@@ -33,6 +34,7 @@ public class Language
 
         // IMP: Need to add new function (FOR)
         naryOperators.add(new ForOperator());
+        naryOperators.add(new WhileDoOperator());
     }
 
     /**
---
+++ src/main/java/lapr4/green/s3/lang/n1140618/WhileDoOperator.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr4.green.s3.lang.n1140618;
+
+import csheets.core.IllegalValueTypeException;
+import csheets.core.Value;
+import csheets.core.formula.Expression;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.lang.model.element.UnknownElementException;
+import lapr4.blue.s1.lang.n1141233.formula.lang.Language;
+import lapr4.gray.s1.lang.n3456789.formula.NaryOperator;
+
+/**
+ *
+ * @author Tiago
+ */
+public class WhileDoOperator implements NaryOperator{
+
+    public WhileDoOperator(){
+    
+    }
+    @Override
+    public Value applyTo(Expression[] operands) throws IllegalValueTypeException {
+        if(operands.length==2){
+            try
+            {
+                Value value;
+                // Get the sequence operator to execute the body as a block
+                
+                NaryOperator naryOperator = Language.getInstance().getNaryOperator("{");
+
+                // Initialization
+                value = operands[0].evaluate();
+
+                // Copy arguments to be executed in each iteration of the loop
+                Expression[] body = Arrays.copyOfRange(operands, 2, operands.length);
+
+                do
+                {
+                    // Loop body instructions
+                    value = naryOperator.applyTo(body);
+                }
+                while (operands[1].evaluate().toBoolean());
+
+                return value; 
+            } catch (csheets.core.formula.lang.UnknownElementException ex) {
+                return new Value(ex);
+            }
+        }
+        return new Value(new IllegalArgumentException("not enough arguments"));
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "WhileDo";
+    }
+
+    @Override
+    public Value.Type getOperandValueType() {
+        return Value.Type.UNDEFINED;
+    }
+    
+}
